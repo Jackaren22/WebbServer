c255f926871086be1d0e98b9a2c90bb8c45c1d7b
Jackaren22 <Jackaren22@gmail.com>
2020-09-01 10:59:08 +0200

    hjälp

--- firstserver/app.js
+++ firstserver/app.js
@@ -2,5 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+const clientDir = __dirname + "\\client\\"
+
+app.get('/', (req, res) => res.sendFile(clientDir + "index2.html"))
+app.get('/style', (req, res) => res.sendFile(clientDir + "story.css"))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
