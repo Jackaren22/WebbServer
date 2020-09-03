a8a2c384006e5a5764e5edaeae76d459b9e9101d
Jackaren22 <Jackaren22@gmail.com>
2020-09-03 12:45:11 +0200

    Första Webbservern
    det är fint fint

--- firstserver/app.js
+++ firstserver/app.js
@@ -5,5 +5,29 @@ const port = 3000
 const clientDir = __dirname + "\\client\\"
 
 app.get('/', (req, res) => res.sendFile(clientDir + "index2.html"))
-app.get('/style', (req, res) => res.sendFile(clientDir + "story.css"))
+
+app.get('/cssen', (req, res) => {
+    res.sendFile(clientDir + "story.css")
+})
+
+app.get('/mqueen', (req, res) => {
+    res.sendFile(clientDir + "mqueen.png")
+})
+
+app.get('/ang', (req, res) => {
+    res.sendFile(clientDir + "äng.jpg")
+})
+
+app.get('/pistol', (req, res) => {
+    res.sendFile(clientDir + "pistol.png")
+})
+
+app.get('/hand', (req, res) => {
+    res.sendFile(clientDir + "hand.png")
+})
+
+app.get('/lizard', (req, res) => {
+    res.sendFile(clientDir + "lizarddance.mp4")
+})
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
