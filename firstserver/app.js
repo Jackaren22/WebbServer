f343ac7a7db300a0aa9d1acc3fbe68f4a1e46ebc
Jackaren22 <Jackaren22@gmail.com>
2020-09-10 12:46:45 +0200

    POST request
    it works now epic!

--- firstserver/app.js
+++ firstserver/app.js
@@ -4,6 +4,9 @@ const port = 3000
 
 const clientDir = __dirname + "\\client\\"
 
+app.use(express.json())
+app.use(express.urlencoded())
+
 app.get('/', (req, res) => res.sendFile(clientDir + "index2.html"))
 
 app.get('/cssen', (req, res) => {
@@ -28,6 +31,12 @@ app.get('/hand', (req, res) => {
 
 app.get('/lizard', (req, res) => {
     res.sendFile(clientDir + "lizarddance.mp4")
+})
+
+app.post('/', (req, res) => {
+  console.log(req.body.username)
+  console.log(req.body.email)
+  res.redirect('/')
 })
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
